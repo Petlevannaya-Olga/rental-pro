ae619ec1a843851bc802f371fa831fda66a1ff94
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-06-16 11:36:14 +0300

    Левое боковое меню

--- src/RentalPro.Presentation.Desktop/ViewModels/MainViewModel.cs
+++ src/RentalPro.Presentation.Desktop/ViewModels/MainViewModel.cs
@@ -1,7 +1,16 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace RentalPro.Presentation.Desktop.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _currentPageTitle = "Дашборд";
+
+    [RelayCommand]
+    private void Navigate(string pageTitle)
+    {
+        CurrentPageTitle = pageTitle;
+    }
 }
